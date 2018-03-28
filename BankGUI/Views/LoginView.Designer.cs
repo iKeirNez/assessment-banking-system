2f544abc37418c7972004dea17ede4b2313a2604
Keir Nellyer <keir@nellyer.co.uk>
2018-03-28 13:58:42 +0100

    Add instructions to login screen

--- BankGUI/Views/LoginView.Designer.cs
+++ BankGUI/Views/LoginView.Designer.cs
@@ -33,12 +33,13 @@
             this.usernameBox = new System.Windows.Forms.TextBox();
             this.passwordBox = new System.Windows.Forms.TextBox();
             this.submitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // usernameLabel
             // 
             this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Location = new System.Drawing.Point(13, 13);
+            this.usernameLabel.Location = new System.Drawing.Point(8, 32);
             this.usernameLabel.Name = "usernameLabel";
             this.usernameLabel.Size = new System.Drawing.Size(50, 13);
             this.usernameLabel.TabIndex = 0;
@@ -47,7 +48,7 @@
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(13, 40);
+            this.passwordLabel.Location = new System.Drawing.Point(8, 59);
             this.passwordLabel.Name = "passwordLabel";
             this.passwordLabel.Size = new System.Drawing.Size(28, 13);
             this.passwordLabel.TabIndex = 1;
@@ -55,22 +56,22 @@
             // 
             // usernameBox
             // 
-            this.usernameBox.Location = new System.Drawing.Point(78, 9);
+            this.usernameBox.Location = new System.Drawing.Point(73, 28);
             this.usernameBox.Name = "usernameBox";
-            this.usernameBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameBox.Size = new System.Drawing.Size(149, 20);
             this.usernameBox.TabIndex = 2;
             // 
             // passwordBox
             // 
-            this.passwordBox.Location = new System.Drawing.Point(78, 36);
+            this.passwordBox.Location = new System.Drawing.Point(73, 55);
             this.passwordBox.Name = "passwordBox";
             this.passwordBox.PasswordChar = '*';
-            this.passwordBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordBox.Size = new System.Drawing.Size(149, 20);
             this.passwordBox.TabIndex = 3;
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(102, 63);
+            this.submitButton.Location = new System.Drawing.Point(147, 81);
             this.submitButton.Name = "submitButton";
             this.submitButton.Size = new System.Drawing.Size(75, 23);
             this.submitButton.TabIndex = 4;
@@ -78,12 +79,22 @@
             this.submitButton.UseVisualStyleBackColor = true;
             this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Please enter your account number and pin.";
+            // 
             // LoginView
             // 
             this.AcceptButton = this.submitButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(191, 94);
+            this.ClientSize = new System.Drawing.Size(233, 114);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.submitButton);
             this.Controls.Add(this.passwordBox);
             this.Controls.Add(this.usernameBox);
@@ -104,6 +115,7 @@
         private System.Windows.Forms.TextBox usernameBox;
         private System.Windows.Forms.TextBox passwordBox;
         private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
